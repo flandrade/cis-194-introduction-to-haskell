33ac5e721228618651a27ee06709cba648ef14de
flandrade <flandradeg@gmail.com>
2016-04-01 09:27:09 -0500

    Week 2 assignment

--- homework02/src/LogAnalysis.hs
+++ homework02/src/LogAnalysis.hs
@@ -24,11 +24,29 @@ import Log
 -- >>> parseMessage "This is not in the right format"
 -- Unknown "This is not in the right format"
 
-parseMessage :: String -> LogMessage
-parseMessage = undefined
+-- Convert String to Int
+getInt :: String -> Int
+getInt x = read x::Int
 
+-- Determine error type
+getErrorType :: String -> MessageType
+getErrorType x = Error $ getInt x
+
+-- List of strings to LogMessage
+getLogMessage :: [String] -> LogMessage
+getLogMessage var = case var of ("E":y:z:xs) -> LogMessage (getErrorType y) (getInt z) (unwords xs)
+                                ("I":y:xs) -> LogMessage Info (getInt y) (unwords xs)
+                                ("W":y:xs) -> LogMessage Warning (getInt y) (unwords xs)
+                                _ -> Unknown "This is not in the right format"
+
+-- Parse message
+parseMessage :: String -> LogMessage
+parseMessage = getLogMessage . words
+
+-- Paser document
 parse :: String -> [LogMessage]
-parse = undefined
+parse x = map parseMessage elements
+    where elements = lines x
 
 ----------------------------------------------------------------------
 -- Exercise 2
@@ -40,7 +58,13 @@ parse = undefined
 --
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) t = t
+insert lm Leaf = Node Leaf lm Leaf
+insert lmInsert@(LogMessage _ timeStampInsert _) mt@(Node left lm@(LogMessage _ timeStamp _) right)
+  | timeStampInsert == timeStamp = mt
+  | timeStampInsert < timeStamp  = Node (insert lmInsert left) lm right
+  | timeStampInsert > timeStamp  = Node left lm (insert lmInsert right)
+
 
 ----------------------------------------------------------------------
 -- Exercise 3
@@ -52,7 +76,9 @@ insert = undefined
 --
 
 build :: [LogMessage] -> MessageTree
-build = undefined
+build [] = Leaf
+build [x] = Node Leaf x Leaf
+build (x:xs) = insert x (build xs)
 
 ----------------------------------------------------------------------
 -- Exercise 4
@@ -64,7 +90,9 @@ build = undefined
 --
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left lm right) = inOrder left ++ [lm] ++ inOrder right
+
 
 ----------------------------------------------------------------------
 -- Exercise 5
@@ -75,8 +103,27 @@ inOrder = undefined
 -- >>>
 --
 
+-- Check if log line is error
+isError :: LogMessage -> Bool
+isError (LogMessage (Error _) _ _) = True
+isError _ = False
+
+-- Check if error is relevant
+isRelevant :: LogMessage -> Bool
+isRelevant (LogMessage (Error n) _ _) = n >= 50
+isRelevant _ = False
+
+-- Get messages according to relevance
+getMessages :: [LogMessage] -> [String]
+getMessages [] = []
+getMessages [Unknown _] = []
+getMessages (lm@(LogMessage _ _ message) : xs)
+  | isError lm && isRelevant lm = message : getMessages xs
+  | otherwise                   = getMessages xs
+
+-- Messages from ordered list
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong = getMessages . inOrder . build
 
 ----------------------------------------------------------------------
 -- Exercise 6 (Optional)
