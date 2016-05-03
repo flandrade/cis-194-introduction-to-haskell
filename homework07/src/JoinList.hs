503dd0c1f35eb117ac3e755f1a4faabde2f530de
flandrade <flandradeg@gmail.com>
2016-05-03 17:47:00 -0500

    Solution to exercise 1

--- homework07/src/JoinList.hs
+++ homework07/src/JoinList.hs
@@ -7,20 +7,36 @@
 
 module JoinList where
 
+import Sized
+
 data JoinList m a = Empty
                   | Single m a
                   | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
 
+-- let x = Single (Product 2) 'j'
+-- let y = Single (Product 7) 'a'
+
 ----------------------------------------------------------------------
 -- Exercise 1
 ----------------------------------------------------------------------
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) listRight listLeft = Append mTop listRight listLeft
+  where mTop = tag listRight `mappend` tag listLeft
+
+tag :: Monoid m => JoinList m a -> m
+tag Empty = mempty
+tag (Single m _) = m
+tag (Append m _ _) = m
 
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
+indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
+indexJ = undefined
 
 ----------------------------------------------------------------------
 -- Exercise 4
