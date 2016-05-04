654882873e869e7893689dde700ce5fe7beea7c9
flandrade <flandradeg@gmail.com>
2016-05-04 17:53:07 -0500

    Solution to exercise 3

--- homework07/src/JoinList.hs
+++ homework07/src/JoinList.hs
@@ -8,6 +8,7 @@
 module JoinList where
 
 import Sized
+import Scrabble
 
 data JoinList m a = Empty
                   | Single m a
@@ -16,6 +17,8 @@ data JoinList m a = Empty
 
 -- let x = Single (Product 2) 'j'
 -- let y = Single (Product 7) 'a'
+-- let b = (+++) (Single (Product 3) 'x') (Append (Product {getProduct = 14}) (Single (Product {getProduct = 2}) 'j') (Single (Product {getProduct = 7}) 'a'))
+-- let b = Append (Product 210) (Append (Product 30) (Single (Product 5) 'y') (Append (Product 6) (Single (Product 2) 'e') (Single (Product 3) 'a'))) (Single (Product 7) 'h')
 
 ----------------------------------------------------------------------
 -- Exercise 1
@@ -26,8 +29,8 @@ data JoinList m a = Empty
   where mTop = tag listRight `mappend` tag listLeft
 
 tag :: Monoid m => JoinList m a -> m
-tag Empty = mempty
-tag (Single m _) = m
+tag Empty          = mempty
+tag (Single m _)   = m
 tag (Append m _ _) = m
 
 
@@ -35,8 +38,51 @@ tag (Append m _ _) = m
 -- Exercise 2
 ----------------------------------------------------------------------
 
+
+getSized :: Sized b => b -> Int
+getSized = getSize . size
+
+getWrappedSize :: (Sized b, Monoid b) => JoinList b a -> Int
+getWrappedSize = getSized . tag
+
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ = undefined
+indexJ _ Empty = Nothing
+indexJ x (Single _ value)
+  | x == 0                      = Just value
+  | otherwise                   = Nothing
+indexJ x (Append s listLeft listRight)
+  | x > getSized s              = Nothing
+  | x < getWrappedSize listLeft = indexJ x listLeft
+  | otherwise                   = indexJ (x - getWrappedSize listLeft) listRight
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ _ Empty = Empty
+dropJ x ls@(Single _ _)
+  | x == 0                      = ls
+  | otherwise                   = Empty
+dropJ x (Append s listLeft listRight)
+  | x > (getSized s)            = Empty
+  | x < getWrappedSize listLeft = dropJ x listLeft
+  | otherwise                   = dropJ (x - getWrappedSize listLeft) listRight
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ _ Empty = Empty
+takeJ x ls@(Single _ _)
+  | x == 0                      = Empty
+  | otherwise                   = ls
+takeJ x ls@(Append s listLeft listRight)
+  | x > (getSized s)            = ls
+  | x < getWrappedSize listLeft = takeJ x listLeft
+  | otherwise                   = (+++) listLeft $ dropJ (x - getWrappedSize listLeft) listRight
+
+
+----------------------------------------------------------------------
+-- Exercise 3
+----------------------------------------------------------------------
+
+scoreLine :: String -> JoinList Score String
+scoreLine s = Single (scoreString s) s
+
 
 ----------------------------------------------------------------------
 -- Exercise 4
--- homework07/src/Scrabble.hs
+++ homework07/src/Scrabble.hs
@@ -4,9 +4,29 @@
 -- CIS 194 Spring 2013: Homework 07
 --
 ----------------------------------------------------------------------
-
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Scrabble where
 
 ----------------------------------------------------------------------
 -- Exercise 3
 ----------------------------------------------------------------------
+
+newtype Score = Score Int deriving (Eq, Ord, Show, Num)
+
+instance Monoid Score where
+  mempty  = Score 0
+  mappend = (+)
+
+score :: Char -> Score
+score letter
+  | letter `elem` "aeilnorstu" = Score 1
+  | letter `elem` "dg"         = Score 2
+  | letter `elem` "bcmp"       = Score 3
+  | letter `elem` "fhvwy"      = Score 4
+  | letter `elem` "k"          = Score 5
+  | letter `elem` "jx"         = Score 8
+  | letter `elem` "qz"         = Score 10
+  | otherwise                  = Score 0
+
+scoreString :: String -> Score
+scoreString = mconcat . map score
