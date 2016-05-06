ca668f829e772ad6bcd4f93aac2686ae41074fbc
flandrade <flandradeg@gmail.com>
2016-05-06 17:33:16 -0500

    change text in main

--- homework07/src/JoinList.hs
+++ homework07/src/JoinList.hs
@@ -106,9 +106,12 @@ instance Buffer (JoinList (Score, Size) String) where
   value (Append (s,_) _ _) = getScore s
 
 -- Run Editor
-main = runEditor editor (fromString $ unlines
+main = runEditor editor content
+
+content :: JoinList (Score, Size) String
+content = fromString $ unlines
          [ "This buffer is for notes you don't want to save, and for"
          , "evaluation of steam valve coefficients."
          , "To load a different file, type the character L followed"
          , "by the name of the file."
-         ] :: JoinList (Score, Size) String)
+         ] :: JoinList (Score, Size) String
