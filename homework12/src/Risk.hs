3d649aaed8f0a13808e1ee307682f67bb4374f95
flandrade <flandradeg@gmail.com>
2016-05-24 18:14:48 -0500

    Add solution to assignment 12

--- homework12/src/Risk.hs
+++ homework12/src/Risk.hs
@@ -3,6 +3,8 @@
 module Risk where
 
 import Control.Monad.Random
+import Control.Applicative
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -34,14 +36,49 @@ data Battlefield =
     { attackers :: Army
     , defenders :: Army
     }
+  deriving (Eq, Ord, Show)
 
 
 ----------------------------------------------------------------------
 -- Exercise 2
 ----------------------------------------------------------------------
 
+-- Replicate function from lecture
+replicateM :: Monad m => Int -> m a -> m [a]
+replicateM n m = sequence (replicate n m)
+
+-- The dice function simulares the roll of @n@ dice.
+dice :: Int -> Rand StdGen [DieValue]
+dice n = replicateM n die
+
+-- Result of battle
 battle :: Battlefield -> Rand StdGen Battlefield
-battle = undefined
+battle x = do
+  attackerDice <- dice (testMax attacker)
+  defenderDice <- dice (testMax defender)
+  let casualties = defineCasualties attackerDice defenderDice
+  return $ resultBattle x casualties
+  where attacker = attackers x
+        defender = defenders x
+        testMax s = if (s >= 2) then 2 else s
+
+-- resultBattle = soldiers - casualties
+resultBattle :: Battlefield -> Battlefield -> Battlefield
+resultBattle soldiers casualties =
+  Battlefield (attackers soldiers - attackers casualties)
+              (defenders soldiers - defenders casualties)
+
+-- Define casualties according to attacker and defender dices
+defineCasualties:: [DieValue] -> [DieValue] -> Battlefield
+defineCasualties attacker defender = Battlefield numAttackers numDefenders
+  where res = defineResult (sort attacker) (sort defender)
+        numDefenders = sum $ map fromEnum res
+        numAttackers= length res - numDefenders
+
+-- Return True if attacker wins
+defineResult :: [DieValue] -> [DieValue] -> [Bool]
+defineResult attacker defender =
+  getZipList $ (>) <$> ZipList  attacker <*> ZipList defender
 
 
 ----------------------------------------------------------------------
@@ -49,7 +86,9 @@ battle = undefined
 ----------------------------------------------------------------------
 
 invade :: Battlefield -> Rand StdGen Battlefield
-invade = undefined
+invade x
+  | attackers x < 2 || defenders x == 0 = return x
+  | otherwise                           = (battle x) >>= invade
 
 
 ----------------------------------------------------------------------
@@ -57,4 +96,7 @@ invade = undefined
 ----------------------------------------------------------------------
 
 successProb :: Battlefield -> Rand StdGen Double
-successProb = undefined
+successProb x = do
+  results <- replicateM 1000 (invade x)
+  let n = length . filter (\b -> (defenders b) == 0) $ results
+  return $ (fromIntegral n) / (fromIntegral 1000)
